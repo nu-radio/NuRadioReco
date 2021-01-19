changes from cluster, correct units

NuRadioReco/examples/cr_analysis/2_threshold_final.py
@@ -97,7 +97,7 @@ trigger_rate = data['trigger_rate']
 
 hardware_response = data['hardware_response']
 
-trigger_thresholds = (np.arange(check_trigger_thresholds[-5], check_trigger_thresholds[-5] + 0.00003, threshold_steps)) *units.volt
+trigger_thresholds = (np.arange(check_trigger_thresholds[-4], check_trigger_thresholds[-4] + 0.00003, threshold_steps)) *units.volt
 
 # print('passband', passband_trigger / units.megahertz)
 # print('checked threshold', check_trigger_thresholds)

NuRadioReco/examples/cr_analysis/3_threshold_average_and_plot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os, sys
 from radiotools import helper as hp
 from NuRadioReco.utilities import units, io_utilities
 import pickle
@@ -17,77 +17,74 @@ passband_low = args.passband_low
 passband_high = args.passband_high
 number_of_files = args.number_of_files
 
-
 n_files = number_of_files
 
-trigger_status = []
-triggered_true = []
-triggered_all = []
-trigger_efficiency = []
-trigger_rate = []
+input_filename = 'output_threshold_final/final_threshold_pb_{:.0f}_{:.0f}_i20000_1.pickle'.format(passband_low,
+                                                                                                      passband_high)
 
-for i_file in range(number_of_files):
-    input_filename = 'output_threshold_final/final_threshold_pb_{:.0f}_{:.0f}_i2000_{}.pickle'.format(passband_low,
-                                                                                                      passband_high,
-                                                                                                      i_file)
-    data = []
-    data = io_utilities.read_pickle(input_filename, encoding='latin1')
-    print(data)
-    trigger_efficiency_one = data['efficiency']
-    trigger_efficiency.append(trigger_efficiency_one)
-    trigger_rate_one = data['trigger_rate']
-    trigger_rate.append(trigger_rate_one)
-    triggered_trigger_one = data['triggered_true']
-    triggered_true.append(triggered_trigger_one)
-    trigger_tested_one = data['triggered_all']
-    triggered_all.append(trigger_tested_one)
-    trigger_status_one = data['trigger_status']
-    trigger_status.append(trigger_status_one)
+data = []
+data = io_utilities.read_pickle(input_filename, encoding='latin1')
 
 detector_file = data['detector_file']
 default_station = data['default_station']
-sampling_rate = data['sampling_rate'] * units.gigahertz
+sampling_rate = data['sampling_rate']
 station_time = data['station_time']
 station_time_random = data['station_time_random']
 hardware_response = data['hardware_response']
 
-Vrms_thermal_noise = data['Vrms_thermal_noise'] * units.volt
-T_noise = data['T_noise'] * units.kelvin
-T_noise_min_freq = data['T_noise_min_freq'] * units.megahertz
-T_noise_max_freq = data['T_noise_max_freq '] * units.megahertz
+Vrms_thermal_noise = data['Vrms_thermal_noise']
+T_noise = data['T_noise']
+T_noise_min_freq = data['T_noise_min_freq']
+T_noise_max_freq = data['T_noise_max_freq ']
 
 galactic_noise_n_side = data['galactic_noise_n_side']
 galactic_noise_interpolation_frequencies_step = data['galactic_noise_interpolation_frequencies_step']
 
 passband_trigger = data['passband_trigger']
 number_coincidences = data['number_coincidences']
-coinc_window = data['coinc_window'] * units.ns
+coinc_window = data['coinc_window']
 order_trigger = data['order_trigger']
 trigger_thresholds = data['threshold']
 n_iterations = data['iteration']
 
+trigger_status = np.zeros((n_files, 20000, len(trigger_thresholds)))
+triggered_true = np.zeros((n_files, len(trigger_thresholds)))
+triggered_all = np.zeros_like(triggered_true)
+trigger_efficiency = np.zeros_like(triggered_true)
+trigger_rate = np.zeros_like(triggered_true)
 
-trigger_efficiency = np.array(trigger_efficiency) # dimension 0: number of files, dimension 1: different thresholds
-trigger_rate = np.array(trigger_rate)
 
-trigger_tested =  np.sum(triggered_all)
-triggered_trigger =  np.sum(triggered_true, axis=0)
+for i_file in range(number_of_files):
+    input_filename = 'output_threshold_final/final_threshold_pb_{:.0f}_{:.0f}_i20000_{}.pickle'.format(passband_low,
+                                                                                                      passband_high,
+                                                                                            i_file)
+    data = []
+    data = io_utilities.read_pickle(input_filename, encoding='latin1')
+    #print(data)
+    trigger_efficiency[i_file] = data['efficiency']
+    trigger_rate[i_file] = data['trigger_rate']
+    triggered_true[i_file]= data['triggered_true']
+    triggered_all[i_file] = data['triggered_all']
+    trigger_status[i_file] = data['trigger_status']
+
+
+triggered_true_all =  np.sum(triggered_true, axis=0)
 trigger_efficiency_all = np.sum(trigger_efficiency, axis=0) / n_files
 trigger_rate_all = np.sum(trigger_rate, axis=0) / n_files
 iterations = n_iterations * n_files
 
-print('trigger tested all', trigger_tested)
-print('triggered true all', triggered_trigger)
+print('triggered_trigger', triggered_true_all)
 print('trigger efficiency all', trigger_efficiency_all)
 print('trigger rate all [Hz]', trigger_rate_all/units.Hz)
+print(trigger_status.shape)
 
 dic = {}
 dic['detector_file'] = detector_file
 dic['default_station'] = default_station
-dic['sampling_rate'] = sampling_rate * units.gigahertz
+dic['sampling_rate'] = sampling_rate
 dic['T_noise'] = T_noise
-dic['T_noise_min_freq'] = T_noise_min_freq * units.megahertz
-dic['T_noise_max_freq '] = T_noise_max_freq * units.megahertz
+dic['T_noise_min_freq'] = T_noise_min_freq
+dic['T_noise_max_freq '] = T_noise_max_freq
 dic['Vrms_thermal_noise'] = Vrms_thermal_noise
 dic['galactic_noise_n_side'] = galactic_noise_n_side
 dic['galactic_noise_interpolation_frequencies_step'] = galactic_noise_interpolation_frequencies_step
@@ -100,13 +97,12 @@ dic['number_coincidences'] = number_coincidences
 dic['iteration'] = iterations
 dic['threshold'] = trigger_thresholds
 dic['trigger_status'] = trigger_status
-dic['triggered_true'] = np.sum(trigger_status)
-dic['triggered_all'] = len(trigger_status)
-dic['efficiency'] = trigger_efficiency
-dic['trigger_rate'] = trigger_rate
+dic['triggered_true'] = triggered_true_all
+dic['efficiency'] = trigger_efficiency_all
+dic['trigger_rate'] = trigger_rate_all
 dic['hardware_response'] = hardware_response
 
-print(dic)
+#print(dic)
 
 with open('results/dict_ntr_pb_{:.0f}_{:.0f}.pickle'.format(passband_trigger[0]/units.megahertz, passband_trigger[1]/units.megahertz),
           'wb') as pickle_out:
@@ -133,14 +129,25 @@ print('efficiency', efficiency)
 print('trigger rate', trigger_rate/units.Hz)
 print('passband', passband_trigger/units.megahertz)
 
+from scipy.interpolate import interp1d
 
-plt.plot(trigger_thresholds/units.microvolt, trigger_rate[0]/units.Hz, marker='x', label= 'Noise trigger rate', linestyle='none')
+
+x = trigger_thresholds[11:23]/units.microvolt
+y = trigger_rate[11:23]/units.Hz
+f1 = interp1d(x, y, kind='cubic')
+print('f1',f1(31.7575))
+xnew = np.linspace(trigger_thresholds[12]/units.microvolt, trigger_thresholds[22]/units.microvolt)
+
+
+plt.plot(trigger_thresholds/units.microvolt, trigger_rate/units.Hz, marker='x', label= 'Noise trigger rate', linestyle='none')
+plt.plot(xnew, f1(xnew), '--', label='cubic, c(31.7575) = {:.2f} Hz'.format(f1(31.7575)))
 #plt.title('passband = {} MHz, iterations = {:.1e}'.format( passband_trigger/units.megahertz, iterations))
 plt.xlabel(r'Threshold [$\mu$V]', fontsize=18)
 plt.hlines(0, trigger_thresholds[0]/units.microvolt, trigger_thresholds[-1]/units.microvolt, label='0 Hz')
 plt.ylabel('Trigger rate [Hz]', fontsize=18)
 plt.tick_params(axis='x', labelsize=16)
 plt.tick_params(axis='y', labelsize=16)
+plt.yscale('log')
 plt.legend()
 plt.tight_layout()
 #plt.show()